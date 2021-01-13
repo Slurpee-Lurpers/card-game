3f08510f0e6ecd65c9b3e12af2cae3f30ce23c6f
Wolfgang-stack <wolfgang.a.mcrae@gmail.com>
2021-01-13 11:40:34 -0500

    now it works with 5 columns and 55 cards!

--- template - Copy (14) - Copy - Copy/js/main.js
+++ template - Copy (14) - Copy - Copy/js/main.js
@@ -1,25 +1,39 @@
 const firstArray = Array.from(document.querySelectorAll('.one'))
 const secondArray = Array.from(document.querySelectorAll('.two'))
 const thirdArray = Array.from(document.querySelectorAll('.three'))
+const fourthArray = Array.from(document.querySelectorAll('.four'))
 
 const containerOne = document.getElementById('theFirst')
 const containerTwo = document.getElementById('theSecond')
 const containerThree = document.getElementById('theThird')
-
+const containerFour = document.getElementById('theFourth')
+const containerFive = document.getElementById('theFifth')
+const containerSix = document.getElementById('theSixth')
 document.addEventListener('click', doStuff)
 
+// window.onload = () => {
+//     firstArray.forEach(e => e.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
+//     secondArray.forEach(e => e.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
+//     thirdArray.forEach(e => e.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
+//     fourthArray.forEach(e => e.style.backgroundColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`)
 
+//     }
 
-let wholeArray = []
+let deckArray = []
 function doStuff(e) {
 
 
     let theFirstPile = Array.from(containerOne.children)
-    console.log(theFirstPile)
+    
     let theSecondPile = Array.from(containerTwo.children)
-    console.log(theSecondPile)
+    
     let theThirdPile = Array.from(containerThree.children)
-    console.log(theThirdPile);
+    
+    let theFourthPile = Array.from(containerFour.children)
+
+    let theFifthPile = Array.from(containerFive.children)
+    
+    
 
     console.log(theFirstPile.concat(theThirdPile, theSecondPile));
         let target
@@ -28,110 +42,69 @@ function doStuff(e) {
          }else if(e.target.classList.contains('card')){
              target = e.target.parentNode
          }
-
-        if (target === containerThree) {
-            wholeArray = theFirstPile.concat(theThirdPile, theSecondPile)
-        } else if (target === containerOne) {
-            wholeArray = theSecondPile.concat(theFirstPile, theThirdPile)
-        } else if (target === containerTwo) {
-            wholeArray = theFirstPile.concat(theSecondPile, theThirdPile)
+        
+        let middleArray = []
+        let topArray = []
+        let bottomArray = []
+        
+        if(target === containerThree){   //middle
+            middleArray = theThirdPile;
+            topArray = theFirstPile.concat(theSecondPile)
+            bottomArray = theFourthPile.concat(theFifthPile)
+            
+        } else if (target === containerOne){  //first
+            middleArray = theFirstPile;
+            topArray = theFourthPile.concat(theFifthPile)
+            bottomArray = theSecondPile.concat(theThirdPile)
+            
+        } else if (target === containerTwo){  //second
+            middleArray = theSecondPile;
+            topArray = theFifthPile.concat(theFirstPile)
+            bottomArray = theThirdPile.concat(theFourthPile)
+            
+        }else if (target === containerFour){  //fourth
+            middleArray = theFourthPile;
+            topArray = theSecondPile.concat(theThirdPile)  
+            bottomArray = theFifthPile.concat(theFirstPile)
+          
+        }else if (target === containerFive){  //fifth
+            middleArray = theFifthPile;
+            topArray = theThirdPile.concat(theFourthPile)
+            bottomArray = theFirstPile.concat(theSecondPile)
+            
         }
-        console.log(wholeArray);
+        deckArray = topArray.concat(middleArray, bottomArray)
+
+        // if(target === containerThree) {
+        //     deckArray = theFirstPile.concat(theThirdPile, theSecondPile)
+        // } else if (target === containerOne) {
+        //     deckArray = theSecondPile.concat(theFirstPile, theThirdPile)
+        // } else if (target === containerTwo) {
+        //     deckArray = theFirstPile.concat(theSecondPile, theThirdPile)
+        // }
+        console.log(deckArray);
 
         
         let x = 1
-        for (i = 0; i <= wholeArray.length - 1; i++) {
+        for (i = 0; i <= deckArray.length - 1; i++) {
+            console.log(x)
             if (x === 1) {
-                containerOne.appendChild(wholeArray[i])
+                containerOne.appendChild(deckArray[i])
                 x += 1;
             } else if (x === 2) {
-                containerTwo.appendChild(wholeArray[i])
+                containerTwo.appendChild(deckArray[i])
                 x += 1
             } else if (x === 3) {
-                containerThree.appendChild(wholeArray[i])
+                containerThree.appendChild(deckArray[i])
+                x += 1
+            } else if (x === 4){
+                containerFour.appendChild(deckArray[i])
+                x += 1
+            }  else if (x === 5){
+                containerFive.appendChild(deckArray[i])
                 x = 1
             }
             
         }
 
-
-
-    }
-
-
-
-//on first click
-//make an array from the three containers and their elements
-
-// if i pick 15 - 21
-// then 15 - 21 cannot change picture and must go to the correct position
-
-
-
-
-//3 ARRAYS
-
-//FIRST, SECOND AND THIRD
-
-//PICK A CARD
-
-//IF ITS IN THE FIRST THEN MAKE AN ARRAY LIKE: ARR[THIRD, FIRST, SECOND]
-//ITS ITS IN THE SECOND THEN : ARR[FIRST, SECOND, THIRD]
-// IF ITS IN THE THIRD THEN ARR[SECOND, THIRD, FIRST]
-
-//THEN ADD THE NEW ARRAY INDECES ONE AT A TIME STARTING AT THE FIRST ARRAY
-//THEN PICK AN ARRAY AND REPEAT
-
-//MAKE ANOTHER ARRAY AND COUNT 11 - THAT WILL BE THE
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let shuffledArray = []
-
-
-// function doOtherStuff(e) {
-//     pickAnArray(e)
-// }
-
-
-
-// function pickAnArray(e) {
-//     if(e.target !== e.currentTarget){
-//         const target = e.target.classList.contains('container') ? e.target : e.target.parentNode;
-//         console.log(target);
-
-
-//     }
-
-// }
+}
